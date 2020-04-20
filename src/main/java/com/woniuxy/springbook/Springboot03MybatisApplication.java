ab6a9d78d2e8261b8603a93eccca37972d0d3c19
zs <zs@163.com>
2020-04-20 11:51:27 +0800

    add zs--aaa

--- src/main/java/com/woniuxy/springbook/Springboot03MybatisApplication.java
+++ src/main/java/com/woniuxy/springbook/Springboot03MybatisApplication.java
@@ -12,6 +12,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 public class Springboot03MybatisApplication {
 
 	public static void main(String[] args) {
+		System.out.println("zs ----aaa");
 		SpringApplication.run(Springboot03MybatisApplication.class, args);
 	}
 
